9e02dc47a7da8691d18e5e304b447b62ee3bba08
Mauricio Paz <mauriciopazdev@gmail.com>
2023-11-03 11:25:59 -0300

    Fixing session

--- context/session.tsx
+++ context/session.tsx
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import { useRouter } from 'next/router';
 import { createContext, useContext, useEffect, useState } from 'react';
 import { bigCommerceSDK } from '../scripts/bcSdk';
@@ -9,12 +10,16 @@ const SessionProvider = ({ children }) => {
     const [context, setContext] = useState('');
 
     useEffect(() => {
-        if (query.context) {
-            setContext(query.context.toString());
+        const signedPayloadJwt = query.signed_payload_jwt;
+        const decodedToken = jwt.decode(signedPayloadJwt, { complete: true });
+        const payload = decodedToken.payload;
+        const context = payload.context;
+        if (context) {
+            setContext(context.toString());
             // Keeps app in sync with BC (e.g. heatbeat, user logout, etc)
-            bigCommerceSDK(query.context);
+            bigCommerceSDK(context);
         }
-    }, [query.context]);
+    }, [query.signed_payload_jwt]);
 
     return (
         <SessionContext.Provider value={{ context }}>
