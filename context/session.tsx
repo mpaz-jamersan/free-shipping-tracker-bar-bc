c5c3081449d920aaa2551a19774f5d53c59370da
Mauricio Paz <146352027+mpaz-jamersan@users.noreply.github.com>
2023-11-03 14:31:07 +0000

    Update 4 files

--- context/session.tsx
+++ context/session.tsx
@@ -1,26 +1,35 @@
-import { useRouter } from 'next/router';
-import { createContext, useContext, useEffect, useState } from 'react';
-import { bigCommerceSDK } from '../scripts/bcSdk';
+import jwt from "jsonwebtoken";
+import { useRouter } from "next/router";
+import { createContext, useContext, useEffect, useState } from "react";
+import { bigCommerceSDK } from "../scripts/bcSdk";
 
-const SessionContext = createContext({ context: '' });
+const SessionContext = createContext({ context: "" });
 
 const SessionProvider = ({ children }) => {
-    const { query } = useRouter();
-    const [context, setContext] = useState('');
+  const { query } = useRouter();
+  const [context, setContext] = useState("");
 
-    useEffect(() => {
-        if (query.context) {
-            setContext(query.context.toString());
-            // Keeps app in sync with BC (e.g. heatbeat, user logout, etc)
-            bigCommerceSDK(query.context);
-        }
-    }, [query.context]);
+  useEffect(() => {
+    const signedPayloadJwt = query.signed_payload_jwt;
+    const decodedToken = jwt.decode(signedPayloadJwt, { complete: true });
+    if (!decodedToken) {
+      return;
+    }
 
-    return (
-        <SessionContext.Provider value={{ context }}>
-            {children}
-        </SessionContext.Provider>
-    );
+    const payload = decodedToken.payload;
+    const context = payload.context;
+    if (context) {
+      setContext(context.toString());
+      // Keeps app in sync with BC (e.g. heatbeat, user logout, etc)
+      bigCommerceSDK(context);
+    }
+  }, [query.signed_payload_jwt]);
+
+  return (
+    <SessionContext.Provider value={{ context }}>
+      {children}
+    </SessionContext.Provider>
+  );
 };
 
 export const useSession = () => useContext(SessionContext);
