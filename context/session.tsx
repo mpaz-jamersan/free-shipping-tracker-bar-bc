36a0feb94d046ee23f2eecef61527e7ef88b3794
Mauricio Paz <mauriciopazdev@gmail.com>
2023-11-03 12:26:49 -0300

    Fixing session

--- context/session.tsx
+++ context/session.tsx
@@ -1,20 +1,23 @@
-import { useRouter } from 'next/router';
+//import { useRouter } from 'next/router';
 import { createContext, useContext, useEffect, useState } from 'react';
 import { bigCommerceSDK } from '../scripts/bcSdk';
 
 const SessionContext = createContext({ context: '' });
 
 const SessionProvider = ({ children }) => {
-    const { query } = useRouter();
+    //const { query } = useRouter();
     const [context, setContext] = useState('');
 
     useEffect(() => {
-        if (query.context) {
-            setContext(query.context.toString());
+        const currentUrl = window.location.href;
+        const urlParams = new URLSearchParams(currentUrl);
+        const context = urlParams.get('context');
+        if (context) {
+            setContext(context.toString());
             // Keeps app in sync with BC (e.g. heatbeat, user logout, etc)
-            bigCommerceSDK(query.context);
+            bigCommerceSDK(context);
         }
-    }, [query.context]);
+    }, []);
 
     return (
         <SessionContext.Provider value={{ context }}>
